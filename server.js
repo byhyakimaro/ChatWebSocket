2a470a18bd94a6391eb1f7e89bab39d9326ecbec
hyakimaro <>
2021-04-22 01:36:06 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+
+//websocket com http
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+//seta o front na pasta public e adicionar html
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/', (req, res) => {
+	res.render('index.html')
+});
+
+//salva as mensagem no array
+let messages = [];
+
+//escuta qualquer conexão nova de socket
+io.on('connection', socket => {
+	console.log(`Socket Novo: ${socket.id}`);
+
+	//recebe as mensagems do front
+	socket.on('sendMessage', data => {
+		messages.push(data);
+		//envia a mensagem para todos conectados no socket
+		socket.broadcast.emit('receivedMessage', data);
+	});
+});
+
+server.listen(3000);
